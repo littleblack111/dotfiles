61e7a62a62de9d596144ce727eba203c9b92adc1
littleblack111 <jerrylam110209@gmail.com>
2023-06-26 16:57:32 +0800

    improved installation script

--- install.sh
+++ install.sh
@@ -1,41 +1,61 @@
 #!/usr/bin/bash
 
 printf "[*] Checking if system have required packages and commands\n"
-if ! command -v git > /dev/null; then
-    printf "[*] git: command not found\n"
-    if command -v pacman > /dev/null; then
-        printf "pacman detected, installing dependencys\n"
-        sudo pacman -S git --noconfirm
-    elif command -v apt-get > /dev/null; then
-        printf "apt-get detected, installing dependencys\n"
-        sudo apt-get -y git
-    elif command -v yum > /dev/null; then
-        printf "yum detected, installing dependencys\n"
-        sudo yum install -y git
-    elif command -v zypper > /dev/null; then
-        printf "zypper detected, installing dependencys\n"
-        sudo zypper install -y git
-    elif command -v emerge > /dev/null; then
-        printf "emerge detected, installing dependencys\n"
-        emerge git
-    elif command -v nix > /dev/null; then
-        printf "nix detected, installing dependencys\n"
-        nix-env -iA nixpkgs.git
-    else
-        printf "please install git with your package manager(git-scm.com/book/en/v2/getting-started-installing-git)\n"
+if command -v gh > /dev/null; then
+    ghc=true
+fi
+# since it's dangours, so removed.
+#if command -v rsync > /dev/null; then
+#    rsyncc=true
+#fi
+if [ $ghc != true ]; then
+    if ! command -v git > /dev/null; then
+        printf "[*] git: command not found\n"
+        if command -v pacman > /dev/null; then
+            printf "pacman detected, installing dependencys\n"
+            sudo pacman -S git --noconfirm
+        elif command -v apt-get > /dev/null; then
+            printf "apt-get detected, installing dependencys\n"
+            sudo apt-get -y git
+        elif command -v yum > /dev/null; then
+            printf "yum detected, installing dependencys\n"
+            sudo yum install -y git
+        elif command -v zypper > /dev/null; then
+            printf "zypper detected, installing dependencys\n"
+            sudo zypper install -y git
+        elif command -v emerge > /dev/null; then
+            printf "emerge detected, installing dependencys\n"
+            emerge git
+        elif command -v nix > /dev/null; then
+            printf "nix detected, installing dependencys\n"
+            nix-env -iA nixpkgs.git
+        else
+            printf "please install git with your package manager(git-scm.com/book/en/v2/getting-started-installing-git)\n"
+            exit 127
+        fi
+    elif ! command -v cp > /dev/null; then
+        printf "[*] cp: command not found\n"
+        exit 127
+    elif ! command -v rm > /dev/null; then
         exit 127
     fi
-elif ! command -v cp > /dev/null; then
-    printf "[*] cp: command not found\n"
-    exit 127
-elif ! command -v rm > /dev/null; then
-    exit 127
 fi
 
 
 printf "[*] Cloning gh repo into /tmp/dots.tmp\n"
-git clone https://github.com/littleblack111/dotfiles.git /tmp/dots.tmp --recurse-submodules || ec=$?; printf "An error had occured during git clone\n"; exit $ec
+if [ $ghc = true ]; then
+    gh repo clone littleblack111/dotfiles /tmp/dots.tmp -- --recurse-submodules || ec=$?; printf "An error had occured during gh repo clone\n"; exit $ec
+else
+    git clone https://github.com/littleblack111/dotfiles.git /tmp/dots.tmp --recurse-submodules || ec=$?; printf "An error had occured during git clone\n"; exit $ec
+fi
+
 printf "[*] Copying configs from /tmp/dots.tmp to $HOME/.config/\n"
+# this will override.
+#if [ $rsyncc = true ]; then
+#    rsync -avxHAXP --exclude '.git*' /tmp/dots.tmp/* ~/.config
+#fi
 cp -vri /tmp/dots.tmp/* $HOME/.config/ || ec=$?; printf "An error had occured during copy\n"; exit $ec
 printf "[*] Deleting temporary files...\n" 
 rm -vrf /tmp/dots.tmp || ec=$?; printf "An error had occured during deletion\n"; exit $ec
+
+exit 0
