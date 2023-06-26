d3aa4967b2655ddbc7efc4941dd0fa56732a8034
littleblack111 <jerrylam110209@gmail.com>
2023-06-26 19:24:56 +0800

    updated install.sh

--- install.sh
+++ install.sh
@@ -94,7 +94,8 @@ printf "[*] Installing configs from /tmp/dots.tmp to $HOME/.config/\n"
 #if [ $rsyncc = true ]; then
 #    rsync -avxHAXP --exclude '.git*' /tmp/dots.tmp/* ~/.config
 #fi
-cp -vri /tmp/dots.tmp/* $HOME/.config/ #|| ec=$?; printf "An error had occured during installation(copy)\n"; exit $ec
+#cp -vri /tmp/dots.tmp/* $HOME/.config/ #|| ec=$?; printf "[!] An error had occured during installation(copy)\n"; exit $ec
+cp -vr /tmp/dots.tmp/* $HOME/.config/ || ec=$?; printf "[!] An error had occured during installation(copy)\n"; exit $ec
 if [ ! -e $HOME/.config/.ver ]; then
     printf "[!] An error had occured during installation(copy)\n"; exit 1
     exit 1
