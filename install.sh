6a4540d71c7cc061e00f5aaa7861350c9e9dc9f9
littleblack111 <jerrylam110209@gmail.com>
2023-06-26 19:32:53 +0800

    updated install.sh

--- install.sh
+++ install.sh
@@ -96,10 +96,10 @@ printf "[*] Installing configs from /tmp/dots.tmp to $HOME/.config/\n"
 #fi
 #cp -vri /tmp/dots.tmp/* $HOME/.config/ #|| ec=$?; printf "[!] An error had occured during installation(copy)\n"; exit $ec
 cp -vr /tmp/dots.tmp/* $HOME/.config/ #|| ec=$?; printf "[!] An error had occured during installation(copy)\n"; exit $ec
-if [ ! -e $HOME/.config/.ver ]; then
-    printf "[!] An error had occured during installation(copy)\n"; exit 1
-    exit 1
-fi
+#if [ ! -e $HOME/.config/.ver ]; then
+#    printf "[!] An error had occured during installation(copy)\n"; exit 1
+#    exit 1
+#fi
 cp -vri /tmp/dots.tmp/.zshrc $HOME || ec=$?; printf "An error had occured during installation(copy .zshrc)\n"; exit $ec
 cp -vri /tmp/dots.tmp/.zshenv $HOME || ec=$?; printf "An error had occured during installation(copy .zshenv)\n"; exit $ec
 printf "[*] Installing stuff that require root privlliage(might prompt for sudo password)"
