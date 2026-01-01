388abd19a69e1997e4eec721a2fab2f931cac4ef
littleblack111 <littleblack11111@gmail.com>
2026-01-01 13:02:00 +0800

    bump: dots

--- dot-config/Thunar/accels.scm
+++ dot-config/Thunar/accels.scm
@@ -2,6 +2,7 @@
 ; this file is an automated accelerator map dump
 ;
 ; (gtk_accel_path "<Actions>/ThunarBookmarks/25078f7c4b1b447498db786655330d9f" "")
+; (gtk_accel_path "<Actions>/ThunarDetailsView/expandable-folders" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
 ; (gtk_accel_path "<Actions>/ThunarStatusBar/toggle-last-modified" "")
 ; (gtk_accel_path "<Actions>/Thunarwindow/menu" "")
@@ -93,6 +94,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-as-icons" "<Primary>1")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/delete-2" "<Shift>Delete")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-in" "<Primary>plus")
+; (gtk_accel_path "<Actions>/ThunarStandardView/configure-columns" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/rename" "F2")
 ; (gtk_accel_path "<Actions>/ThunarBookmarks/269652eb42aca936b9f6c0740d553c7d" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-location" "<Primary>l")
